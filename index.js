994495dc1a16d2c9e59eab936bd00362b6aac8bb
rschlosser11 <rosalinschlosser@gmail.com>
2020-07-21 15:21:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your functions here
+function map(arr, func) {
+    return arr.map(func)
+}
+
+function reduce(arr, func, start = 0) {
+    if (start === 0) {
+        return arr.reduce(func)
+    } else {
+        return arr.reduce(func, start)
+    }
+}
